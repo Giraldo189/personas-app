aa089d233ef53d273f4b273e306a2d13769f2cb4
Giraldo189 <miguelgiraldo200@gmail.com>
2024-03-29 03:58:15 -0500

    Boton Eliminar

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -99,6 +99,13 @@ class ComunaController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $comuna = Comuna::find($id);
+        $comuna->delete();
+
+        $comunas = DB::table('tb_comuna')
+        ->join('tb_minicipio','tb_comuna.muni_codi','=','tb_municipio.municodi')
+        ->select('tb_comuna.*','tb_municipio.muni_nomb')
+        ->get();
+        return view("comuna.index",["comunas"=>$comunas]);
     }
 }
