e98bb74becbea0975a96d4f97b01f7115814656f
Giraldo189 <miguelgiraldo200@gmail.com>
2024-03-29 02:15:29 -0500

    Columna (Municipio)

--- resources/views/comuna/index.blade.php
+++ resources/views/comuna/index.blade.php
@@ -29,8 +29,8 @@
     @foreach ($comunas as $comuna)
     <tr>
       <th scope="row">{{$comuna -> comu_codi }}</th>
-      <td>{{$comuna -> comu_nomb }}</td>
-      <td>{{$comuna -> muni_nomb }}</td>
+      <td>{{$comuna ->comu_nomb }}</td>
+      <td>{{$comuna ->muni_nomb }}</td>
       <td><span>Actions </span></td>
     </tr>
     @endforeach
