26de3fc1b7d8a3151b2fafe52926fcbd4b7aed05
Giraldo189 <miguelgiraldo200@gmail.com>
2024-04-02 01:33:10 -0500

    Municipios - Departamentos

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ComunaController;
+use App\Http\Controllers\MunicipioController;
+use App\Models\Municipio;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,3 +26,11 @@ Route::get('/comunas/create',[ComunaController::class,'create'])->name('comunas.
 Route::delete('/comunas/{comuna}',[ComunaController::class,'destroy'])->name('comunas.destroy');
 Route::put('/comunas/{comuna}',[ComunaController::class,'update'])->name('comunas.update');
 Route::get('/comunas/{comuna}/edit',[ComunaController::class,'edit'])->name('comunas.edit');
+
+// Ruta de los Municipios 
+Route::get('/municipios',[MunicipioController::class,'index'])->name('municipio.index');
+Route ::post('/municipios',[MunicipioController::class,'store'])->name('municipios.store');
+Route ::get('/municipios/create',[MunicipioController::class,'create'])->name('municipios.create');
+Route ::put('/municipios/{municipio}',[MunicipioController::class,'update'])->name('municipios.update');
+Route ::get('/municipios/{municipio}/edit',[MunicipioController::class,'edit'])->name('municipios.edit');
+Route ::delete('/municipios/{municipio}',[MunicipioController::class,'destroy'])->name('municipios.destroy');
